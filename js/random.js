8bbe0e7c701da553b80d048cf1be52caad0cb3bf
Pýrbor <111013695+Pyrbor@users.noreply.github.com>
2024-05-06 19:26:49 -0300

    Updated the random redirect script

--- js/random.js
+++ js/random.js
@@ -1,21 +1,11 @@
-function fetchDirectoryList(directoryPath, callback) {
-  fetch(directoryPath)
-      .then(response => response.text())
-      .then(html => {
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(html, 'text/html');
-          const links = Array.from(doc.querySelectorAll('a[href]'));
-          const fileList = links.map(link => link.getAttribute('href'));
-          callback(fileList);
-      })
-      .catch(error => console.error('Error fetching directory list:', error));
-}
+function redirectToRandomPage() {
+  
+  // add the name of the pages in /a/ here
+  var pageNames = [ 
+    "example"
+  ];
 
-function redirectToRandomPage(directoryList) {
-  const randomIndex = Math.floor(Math.random() * directoryList.length);
-  const randomPage = directoryList[randomIndex];
-  const rootPath = window.location.origin;
-  window.location.href = rootPath + '/a/' + randomPage;
+  var randomIndex = Math.floor(Math.random() * pageNames.length);
+  var url = "/a/" + pageNames[randomIndex] + ".html";
+  window.location.href = "https://encycloplate.github.io" + url;
 }
-
-fetchDirectoryList('/a/', redirectToRandomPage);
